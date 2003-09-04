876670ccdd0712e5660faccf4d8a15a2e7307ef7
user57 <user57@ab1eb0d7-402a-0410-9a25-f38f790f4277>
2003-09-04 19:05:03 +0000

    o Create a latest.html file when opening a new log which redirects the user    to the latest file ala javascript

--- jerk/src/java/main/com/werken/jerk/services/log/HtmlLogger.java
+++ jerk/src/java/main/com/werken/jerk/services/log/HtmlLogger.java
@@ -50,9 +50,12 @@ import com.werken.jerk.Channel;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.RandomAccessFile;
 import java.io.InputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
@@ -141,6 +144,30 @@ public class HtmlLogger extends AbstractLogger
             }
             
             reader.close();
+        }
+        else
+        {
+            // Its a new file, update the latest file
+            File latestFile = new File(logFile.getParentFile(), "latest.html");
+            log.debug("Writing new latest file: " + latestFile);
+            PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(latestFile)));
+            
+            try {
+                writer.println("<html><head>");
+                writer.println("<script language=\"javascript\">");
+                writer.println("<!--");
+                writer.println("document.location.replace(\"" + logFile.getName() + "\");");
+                writer.println("//-->");
+                writer.println("</script>");
+                writer.println("<body>");
+                writer.println("You should have been redirected ");
+                writer.println("<a href=\"" + logFile.getName() + "\">here</a>.");
+                writer.println("</body></html>");
+                writer.flush();
+            }
+            finally {
+                writer.close();
+            }
         }
         
         this.out = new RandomAccessFile( this.logFile, "rw" );
